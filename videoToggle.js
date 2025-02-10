f6387c52494a0435963179553a3df1ac891bf506
Rosendo99er <stephan.vila@cream-collection.de>
2025-02-10 04:21:38 +0100

    Delete videoToggle.js

--- videoToggle.js
+++
@@ -1,8 +0,0 @@
-export function toggleVideoContainer(consentGiven) {
-    const videoContainer = document.getElementById('video-container');
-    if (consentGiven) {
-      videoContainer.style.display = 'none'; // Video-Container ausblenden
-    } else {
-      videoContainer.style.display = 'block'; // Video-Container wieder anzeigen
-    }
-  }
